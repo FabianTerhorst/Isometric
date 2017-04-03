61ac657c2e634d3bd5542c45dfd2d51f22ff542f
fabianterhorst <fabian.terhorst@gmail.com>
2017-04-03 16:11:44 +0200

    * update sample app

--- app/src/main/java/io/fabianterhorst/isometric/sample/MainActivity.java
+++ app/src/main/java/io/fabianterhorst/isometric/sample/MainActivity.java
@@ -6,10 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import io.fabianterhorst.isometric.Color;
 import io.fabianterhorst.isometric.IsometricView;
 import io.fabianterhorst.isometric.Point;
-import io.fabianterhorst.isometric.shapes.Octahedron;
 import io.fabianterhorst.isometric.shapes.Prism;
-import io.fabianterhorst.isometric.shapes.Pyramid;
-import io.fabianterhorst.isometric.shapes.Stairs;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,10 +20,22 @@ public class MainActivity extends AppCompatActivity {
         //isometricView.setSort(false);
         //isometricView.add(new Cylinder(Point.ORIGIN, 3d, 3d, 1d), new Color(160, 60, 50));
         //isometricView.add(new Prism(Point.ORIGIN, 3d, 3d, 1d), new Color(160, 60, 50));
-        isometricView.add(new Prism(new Point(2, 4, 0), 3d, 3d, 1d), new Color(160, 60, 50));
+
+        /*isometricView.add(new Prism(new Point(2, 4, 0), 3d, 3d, 1d), new Color(160, 60, 50));
         isometricView.add(new Prism(new Point(2, 0, 1), null, null, null), new Color(50, 60, 160));
         isometricView.add(new Octahedron(new Point(2,2,1)), new Color(0,180,180));
         isometricView.add(new Pyramid(new Point(2, 2, 0), null, null, null), new Color(180,180,0));
-        isometricView.add(new Stairs(new Point(2,3, 0), 10), new Color(180, 180, 0));
+        isometricView.add(new Stairs(new Point(2,3, 0), 10), new Color(180, 180, 0));*/
+        sampleTwo(isometricView);
+    }
+
+    public void sampleOne(IsometricView isometricView) {
+        isometricView.add(new Prism(new Point(0, 0, 0)), new Color(33, 150, 243));
+    }
+
+    public void sampleTwo(IsometricView isometricView) {
+        isometricView.add(new Prism(new Point(0, 0, 0), 4, 4, 2), new Color(33, 150, 243));
+        isometricView.add(new Prism(new Point(-1, 1, 0), 1, 2, 1), new Color(33, 150, 243));
+        isometricView.add(new Prism(new Point(1, -1, 0), 2, 1, 1), new Color(33, 150, 243));
     }
 }
